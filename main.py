bc10571d3d356f4fdf999dcc1b8ffb239c634018
Anton Kuznetsov <kuzantiv1992@mail.ru>
2022-12-18 12:47:03 +0600

    added import

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from random import randint
 from graphic_arts.start_game_banner import run_screensaver
 
-
 def attack(char_name: str, char_class: str) -> str:
+    """Perform attack."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +15,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Perform defence."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -24,6 +25,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Perform special power."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -34,6 +36,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Perform training."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -58,6 +61,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Perform choise of character."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
